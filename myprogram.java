3c54797cf7e9c979a717d3486ba2ee5a831a2998
faze flynn <faiz.14a@gmail.com>
2024-02-06 14:09:09 +0530

    this is my first commit

---
+++ myprogram.java
@@ -0,0 +1,11 @@
+
+public class myprogram {
+
+    public static void main (String[] args){
+        myprogram t = new myprogram();
+
+
+        System.out.println("hello this is my first program ");
+       
+    }
+}
