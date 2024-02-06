b54a3c3994d68e2f505027c00b5d94be0bfeadf2
faze flynn <faiz.14a@gmail.com>
2024-02-06 14:12:56 +0530

    second commit

--- myprogram.java
+++ myprogram.java
@@ -2,10 +2,11 @@
 public class myprogram {
 
     public static void main (String[] args){
-        myprogram t = new myprogram();
+        // myprogram t = new myprogram();
 
 
         System.out.println("hello this is my first program ");
+        System.out.println("hello this is my second commit");
        
     }
 }
